78777eb201179987a1498550c8f7c51641e68639
Gergely Karolyi <gergelykarolyi@gmail.com>
2021-03-02 18:10:25 +0100

    deleted unnecessary JS plugin

--- app/javascript/plugins/image_upload.js
+++
@@ -1,8 +0,0 @@
-const submitImgOnUpload = () => {
-  const uploadedImg = document.getElementById("search_photo");
-  if (uploadedImg && uploadedImg.value != "") {
-    uploadedImg.submit();
-  }
-};
-
-export { submitImgOnUpload };
