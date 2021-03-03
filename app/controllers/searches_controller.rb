658fc9c3e4f802f8809ad352233351e6c3af746d
Gergely Karolyi <gergelykarolyi@gmail.com>
2021-03-03 15:45:21 +0100

    finally working

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,24 +1,37 @@
 class SearchesController < ApplicationController
   skip_before_action :authenticate_user!
-  before_action :check_for_params, only: :new
 
   def new
+    set_instance_vars
   end
 
   def create
-    raise
-    @search = Search.new(search_params)
-    @result = Result.new
-    @search.result = @result
+    query = params[:queries] ? "#{params[:queries]} #{params[:search][:query]}" : params[:search][:query]
+    @search = Search.new(query: query)
     @search.user = current_user if user_signed_in?
-    @search.save ? (redirect_to root_path({ query: @search.query })) : (render :new)
+    if @search.save
+      redirect_to root_path(construct_query)
+    else
+      render :new
+    end
   end
 
   private
 
-  def check_for_params
-    @user_params = params.except(:controller, :action)
-    @user_params.empty? ? no_params : set_vars_from_params
+  def construct_query
+    if params[:search][:photo]
+      search_params
+    else
+      { search: { queries: params[:search][:query] + (params[:queries] || "") } }
+    end
+  end
+
+  def user_params
+    params.except(:controller, :action)
+  end
+
+  def set_instance_vars
+    user_params.empty? || user_params[:photo] ? no_params : set_vars_from_params
     @search = Search.new
   end
 
@@ -28,17 +41,23 @@ class SearchesController < ApplicationController
   end
 
   def set_vars_from_params
-    full_query = @user_params.as_json.map { |key, _value| params[key] }.join('&')
-    @query = full_query.split('&')
-    @results = find_or_create_search(full_query)
+    @query = "&" + user_params[:search][:queries].strip
+    @results = search_results(full_query)
   end
 
-  def find_or_create_search(query)
+  def full_query
+    user_params
+      .as_json
+      .map { |key, _value| params[key] }
+      .join('&')
+  end
+
+  def search_results(query)
     search = Search.find_by(query: query) || Search.create(query: query)
     JSON.parse(search.result.json)
   end
 
   def search_params
-    params.require(:search).permit(:query, :user, :result, :photo)
+    params.require(:search).permit(:query, :photo)
   end
 end
