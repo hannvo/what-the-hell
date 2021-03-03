63b4c1f172ed495f2e956c19f67ae55fe0cdf5fd
Gergely Karolyi <gergelykarolyi@gmail.com>
2021-03-03 13:24:54 +0100

    saving progress

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,11 +1,12 @@
 class SearchesController < ApplicationController
   skip_before_action :authenticate_user!
+  before_action :check_for_params, only: :new
 
   def new
-    @search = Search.new(search_params) || Search.new
   end
 
   def create
+    raise
     @search = Search.new(search_params)
     @result = Result.new
     @search.result = @result
@@ -14,6 +15,28 @@ class SearchesController < ApplicationController
   end
 
   private
+
+  def check_for_params
+    @user_params = params.except(:controller, :action)
+    @user_params.empty? ? no_params : set_vars_from_params
+    @search = Search.new
+  end
+
+  def no_params
+    @query = []
+    @results = []
+  end
+
+  def set_vars_from_params
+    full_query = @user_params.as_json.map { |key, _value| params[key] }.join('&')
+    @query = full_query.split('&')
+    @results = find_or_create_search(full_query)
+  end
+
+  def find_or_create_search(query)
+    search = Search.find_by(query: query) || Search.create(query: query)
+    JSON.parse(search.result.json)
+  end
 
   def search_params
     params.require(:search).permit(:query, :user, :result, :photo)
--- app/models/search.rb
+++ app/models/search.rb
@@ -2,4 +2,17 @@ class Search < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :result
   has_one_attached :photo
+  before_validation :fake_results
+
+  private
+
+  def fake_results
+    fake_json = {
+      title: "My fake movie",
+      year: 2006,
+      thumb: 'http://my.image.url.com',
+      db_id: 1123
+    }.to_json
+    self.result = Result.new(json: fake_json)
+  end
 end
