5eb0774eabb69525c8c0780425f195be0771e555
Hannah Voget <hannah@voget.de>
2021-03-04 14:51:47 +0100

    styled and clickable :)

--- app/javascript/plugins/autocomplete.js
+++ app/javascript/plugins/autocomplete.js
@@ -1,15 +1,29 @@
 // display movies in divs
 // if you click a movie the form will be sent with movie id?
 //
+const form = document.getElementById('new_search');
 const movieInput = document.getElementById('search_query');
 const movieSuggestions = document.getElementById('movie-suggestions');
 
+const submitMovie = (event) => {
+  console.log(form);
+  // set the value of the form field to the movie id
+  movieInput.value = event.currentTarget.attributes["movie-id"].value;
+  form.submit();
+}
+
 const insertMovies = (data) => {
-  console.log(data);
   const firstFour = data.results.slice(0,4);
   firstFour.forEach((movie) => {
-    const title = `<p>${movie.original_title}</p>`;
-    movieSuggestions.insertAdjacentHTML('beforeend', title);
+    const movieCard = `<div class="film-option" movie-id="${movie.id}">
+                          <img src="https://image.tmdb.org/t/p/w200/${movie.poster_path}" alt="">
+                          <p>${movie.original_title} (${movie.release_date.slice(0,4)})</p>
+                      </div>`;
+    movieSuggestions.insertAdjacentHTML('beforeend', movieCard);
+    const movieChoice = movieSuggestions.querySelectorAll('.film-option');
+    movieChoice.forEach((movie) => {
+      movie.addEventListener('click', submitMovie);
+    });
  });
 };
 
