8dab18124d31dc19b5752185a83d4e9f5f01905f
Hannah Voget <hannah@voget.de>
2021-03-08 19:30:43 +0100

    actor match works for all cases

--- app/services/tmdb.rb
+++ app/services/tmdb.rb
@@ -4,13 +4,8 @@ class Tmdb
   @api_key = ENV['TMDB_KEY']
 
   def self.get_actors(movie_id)
-    actors_url = "https://api.themoviedb.org/3/movie/#{movie_id}/credits?api_key=#{@api_key}&language=en-US"
+    filtered = api_call_for_actors(movie_id)
 
-    actors_response = URI.parse(actors_url).read
-    actors = JSON.parse(actors_response)["cast"]
-    filtered = actors.select do |actor|
-      actor["known_for_department"] == "Acting"
-    end
     top_actors = filtered.sort do |b, a|
       a["popularity"] - b["popularity"]
     end
@@ -38,9 +33,31 @@ class Tmdb
     movies
   end
 
+  def self.matching_cast(array_of_movie_ids)
+    # get cast to have 2 arrays with [ actor, actor, actr, ...]
+    # arr-o-m-i ["1232", '1232']
+    first_movie_id = array_of_movie_ids.first
+    first_movie_actors_ids = api_call_for_actors(first_movie_id.to_i).map { |actor| actor["id"] }
+
+    second_movie_id = array_of_movie_ids[1]
+    second_movie_actors_ids = api_call_for_actors(second_movie_id.to_i).map { |actor| actor["id"] }
+
+    first_movie_actors_ids & second_movie_actors_ids
+  end
+
   def self.get_actor_details(actor_id)
     # takes an actor ID and returns the JSON response
     url = "https://api.themoviedb.org/3/person/#{actor_id}?api_key=#{@api_key}&language=en-US&include_adult=false"
     URI.parse(url).read
+  end
+
+  def self.api_call_for_actors(movie_id)
+    actors_url = "https://api.themoviedb.org/3/movie/#{movie_id}/credits?api_key=#{@api_key}&language=en-US"
+
+    actors_response = URI.parse(actors_url).read
+    actors = JSON.parse(actors_response)["cast"]
+    actors.select do |actor|
+      actor["known_for_department"] == "Acting"
+    end
   end
 end
