07366683892b2a0ac5cad525303ec87428bd308a
Hannah Voget <hannah@voget.de>
2021-03-10 11:32:39 +0100

    made calculation a little bit better refactoring wise

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -53,32 +53,22 @@ class SearchesController < ApplicationController
   def set_vars_from_params
     # @query [ "123", "1231", "123" ] is an array that stores the ids we need to use to call the api
     @query = user_params[:search][:queries].strip.split("&")
-    if @query.count > 1 && (actor_ids = Tmdb.matching_cast(@query))
-      if actor_ids.count == 1
-        # if we have 2 movie inputs and one matching actor
-        redirect_to result_path(Result.create(json: actor_ids.first))
-      elsif actor_ids.count > 1
-        # if we have 2 movie inputs and several matching actors they
-        # will be displayed on the preresults page
-        cast = []
-        actor_ids.each do |actor_id|
-          cast << Result.create(json: Tmdb.get_actor_details(actor_id))
-        end
-        movies = Tmdb.get_movie_details(@query)
-        @results = { cast: cast, movies: movies }
-        @common_actors = true
-      else
-        cast = Tmdb.get_actors(@query.last.to_i)
-        movies = Tmdb.get_movie_details(@query)
-        @results = { cast: cast, movies: movies }
-        @common_actors = false
-      end
+
+    if @query.count > 1 && (actor_ids = Tmdb.matching_cast(@query)) && actor_ids.count == 1
+      # if we have 2 movie inputs and one matching actor
+      redirect_to result_path(Result.create(json: actor_ids.first))
+    elsif @query.count > 1 && (actor_ids = Tmdb.matching_cast(@query)) && actor_ids.count > 1
+      # if we have 2 movie inputs and several matching actors
+      cast = actor_ids.map { |actor_id| Result.create(json: Tmdb.get_actor_details(actor_id)) }
+      @common_actors = true
     else
       # if there is only 1 movie input or no matching actors display preresults page
       cast = Tmdb.get_actors(@query.last.to_i)
-      movies = Tmdb.get_movie_details(@query)
-      @results = { cast: cast, movies: movies }
+      @common_actors = false
     end
+
+    movies = Tmdb.get_movie_details(@query)
+    @results = { cast: cast, movies: movies }
     # @results = search_results(params[:search][:queries])
   end
 
