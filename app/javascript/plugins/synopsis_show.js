08b1cbda60244c4bf563fbbde76b3c085596a719
Hannah Voget <hannah@voget.de>
2021-03-11 20:12:20 +0100

    styled the recommend movie details

--- app/javascript/plugins/synopsis_show.js
+++ app/javascript/plugins/synopsis_show.js
@@ -1,18 +1,20 @@
 const show = (event) => {
+  const posters = document.querySelectorAll(".reco-poster");
+  posters.forEach((poster) => {poster.style.border = "1px solid #fff";});
+  event.currentTarget.style.border = "1px solid #f4cb6d";
   const movieRecData = event.currentTarget.dataset;
-  console.log(movieRecData);
-const title = `${movieRecData.recTitle} <span class="movie-rec-year">(${movieRecData.recYear.slice(0,4)})</span>`;
+  const title = `${movieRecData.recTitle} <span class="movie-rec-year">(${movieRecData.recYear.slice(0,4)})</span>`;
   document.getElementById("reco-movie-title").innerHTML = title ;
   document.getElementById("reco-movie-description").innerText = movieRecData.recDescription;
 };
 
 const initSynopsisShow = () => {
   const posters = document.querySelectorAll(".reco-poster");
-    console.log(posters);
   if (posters) {
     posters.forEach((poster) => {
       poster.addEventListener("click", show);
     });
+    posters[0].click();
   }
 };
 
